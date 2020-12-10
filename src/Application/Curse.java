3a323f1764a96bf9fde4c299e55e4b85f9e31709
Marcelo1985-06 <marceloliveira_lima@outlook.com>
2020-12-10 11:22:46 -0300

    Dando bom dia ao projeto

--- src/Application/Curse.java
+++ src/Application/Curse.java
@@ -4,5 +4,6 @@ public class Curse {
     public static void main(String[] args) {
 
         System.out.println("Mensagem explicativa");
+        System.out.println("Bom dia, meu projeto");
     }
 }
